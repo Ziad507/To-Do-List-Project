cabff7683ecc45c3138e74e10744433fe0c2c15f
Ziad <Ziadyaser21@gmail.com>
2024-07-10 09:38:39 +0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,10 @@
   plugins: [],
 }').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("tailwindcss"), require("autoprefixer")],
+  plugins: [],
 };
+
